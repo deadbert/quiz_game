88299020c5125f210d33d5bf42aead81d1e98b97
Nick_Spencer <strangebrew202@gmail.com>
2022-08-24 12:39:57 -0600

    set up layout for GUI but not interactive yet

--- ui.py
+++ ui.py
@@ -1,3 +1,32 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
 
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Quizzler')
+        self.window.config(pady=20, padx=20, background=THEME_COLOR)
+
+        self.score_label = Label(text='Score: 0', background=THEME_COLOR, foreground='white')
+        self.score_label.grid(column=1, row=0)
+
+        self.question_box = Canvas(height=250, width=300, bg='white')
+        self.question_text = self.question_box.create_text(
+            150,
+            125,
+            text='Question goes here',
+            font=('Arial', 20, 'italic')
+        )
+        self.question_box.grid(column=0, row=1, columnspan=2, pady=50)
+
+        tru_image = PhotoImage(file='images/true.png')
+        self.true_button = Button(image=tru_image, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_image = PhotoImage(file='images/false.png')
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
