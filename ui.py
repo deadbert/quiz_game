4379c4c564a3b7a9b37aade96acad0c7e60e7bd3
Nick_Spencer <strangebrew202@gmail.com>
2022-08-24 21:13:59 -0600

    Added answer methods to UI that pass user selection of True/False as an argument to check_answer method. Added user_feedback method to flash screen green for correct answers or red for incorrect.

--- ui.py
+++ ui.py
@@ -25,11 +25,11 @@ class QuizInterface:
         self.question_box.grid(column=0, row=1, columnspan=2, pady=50)
 
         tru_image = PhotoImage(file='images/true.png')
-        self.true_button = Button(image=tru_image, highlightthickness=0, command=self.get_next_question)
+        self.true_button = Button(image=tru_image, highlightthickness=0, command=self.answer_true)
         self.true_button.grid(column=0, row=2)
 
         false_image = PhotoImage(file='images/false.png')
-        self.false_button = Button(image=false_image, highlightthickness=0, command=self.get_next_question)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.answer_false)
         self.false_button.grid(column=1, row=2)
 
         self.get_next_question()
@@ -38,3 +38,19 @@ class QuizInterface:
 
     def get_next_question(self):
         self.question_box.itemconfig(self.question_text, text=self.quiz.next_question())
+        self.question_box.config(background='white')
+
+    def answer_true(self):
+        self.user_feedback(self.quiz.check_answer(True))
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+
+    def answer_false(self):
+        self.user_feedback(self.quiz.check_answer(False))
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+
+    def user_feedback(self, answer: bool):
+        if answer:
+            self.question_box.config(background='green')
+        else:
+            self.question_box.config(background='red')
+        self.window.after(500, self.get_next_question)
