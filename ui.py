1b4e5f48a1e9857798a2571ebe6fd69899f6042c
Nick_Spencer <strangebrew202@gmail.com>
2022-08-24 19:54:09 -0600

    Added QuizBrain object as argument for ui object, built get_next_question method in ui to cycle through questions when true/false buttons are clicked

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
-
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
+
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.config(pady=20, padx=20, background=THEME_COLOR)
@@ -16,17 +18,23 @@ class QuizInterface:
         self.question_text = self.question_box.create_text(
             150,
             125,
+            width=280,
             text='Question goes here',
             font=('Arial', 20, 'italic')
         )
         self.question_box.grid(column=0, row=1, columnspan=2, pady=50)
 
         tru_image = PhotoImage(file='images/true.png')
-        self.true_button = Button(image=tru_image, highlightthickness=0)
+        self.true_button = Button(image=tru_image, highlightthickness=0, command=self.get_next_question)
         self.true_button.grid(column=0, row=2)
 
         false_image = PhotoImage(file='images/false.png')
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.get_next_question)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.question_box.itemconfig(self.question_text, text=self.quiz.next_question())
